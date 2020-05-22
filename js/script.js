89f62e1ba17f0a391a2aa03bda4f81679ddd72fa
artem.dubovov <64221301+ArtemDubovov@users.noreply.github.com>
2020-05-22 15:44:03 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        infinite: true,
-        speed: 1200,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-        nextArrow:'<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-      });
-  });
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    responsive: {
+        900: {
+            prevButton: false
+        }
+    }
+});
+
+document.querySelector('.prev').onclick = function () {
+    slider.goTo('prev');
+  };
+document.querySelector('.next').onclick = function () {
+    slider.goTo('next');
+  };
